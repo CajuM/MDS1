b1515d7fbb33ed26cc3136e103f437d0175f74b1
Mihai-Drosi Caju <ddan.dcaju@gmail.com>
2017-06-13 17:28:42 +0300

    Should work now

--- src/main/java/ro/unibuc/fmi/my/mds1/Main.java
+++ src/main/java/ro/unibuc/fmi/my/mds1/Main.java
@@ -23,14 +23,6 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws Exception {
-		Main.APP_HOME = Paths.get(
-			Main.class
-			.getProtectionDomain()
-			.getCodeSource()
-			.getLocation()
-			.toString()
-		).getParent().getParent();
-
 		Properties props = System.getProperties();
 		props.setProperty("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
 
